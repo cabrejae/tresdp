7bdd0be08202093fc97705f7e9bfd3c25c8a6614
Pilu <cabrejae@gmail.com>
2025-04-02 06:32:30 -0300

    prueba a conexion a MySQL Ok

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,21 @@ import pool from "./db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
-// Middleware
 app.use(express.json()); // Para leer JSON en requests
 app.use(cors()); // Habilita CORS (útil si frontend y backend están separados)
 
 // Ruta de prueba para verificar conexión a la base de datos
-app.get("/test-db", async (req, res) => {
+app.get("/test", async (req, res) => {
     try {
-        const [rows] = await pool.query("SELECT p.ID, p.post_title AS nombre, s.meta_value AS stock FROM wpot_posts p JOIN wpot_postmeta s ON p.ID = s.post_id");
-        res.json({ success: true, result: rows[0].result });
+        const [rows] = await pool.query(`SELECT p.ID, p.post_title AS nombre, s.meta_value AS stock
+            FROM wpot_posts p
+            JOIN wpot_postmeta s ON p.ID = s.post_id
+            WHERE p.post_type = 'product' AND s.meta_key = '_stock'
+        `);
+        res.json({ success: true, result: rows }); // Devuelve todas las filas
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
     }
